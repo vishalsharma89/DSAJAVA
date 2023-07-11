a972eebedf2d4d39168585bd5487decf45c2feb8
Asus <sharmavishal9643@gmail.com>
2023-07-11 00:37:04 -0700

    Check array sorted or not using recursion and Iteration

--- untitled/src/RecursionQuestions/ArraySortedOrNot.java
+++ untitled/src/RecursionQuestions/ArraySortedOrNot.java
@@ -2,10 +2,12 @@ package RecursionQuestions;
 
 public class ArraySortedOrNot {
     public static void main(String[] args) {
-int[] arr = {20,21,451,89,91};
+int[] arr = {20,21,41,89,9};
         System.out.println(isSorted(arr,arr.length-1));
+        System.out.println(isSortedI(arr,arr.length-1));
     }
 
+//    USing Recursion
     public static boolean isSorted(int[] arr,int n){
 //        base condition
         if(n==0 || n==1){
@@ -15,5 +17,19 @@ int[] arr = {20,21,451,89,91};
             return false;
         }
         return isSorted(arr,n-1);
+    }
+
+//    Using Iteration
+    public static boolean isSortedI(int[] arr,int n){
+//        base condition
+        if(n==0 || n==1){
+            return true;
+        }
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
     }
 }
